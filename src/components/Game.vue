55c2b33fc8f9e6a3921d0bb639400332efb51659
박영호 <mellonia.bluerose@gmail.com>
2018-08-04 00:58:23 +0900

    프로젝트 초기화

---
+++ src/components/Game.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Game Component
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Game',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div
+      class="title">
+      감자 코인
+    </div>
+    <button
+      class="startButton"
+      type="button"
+      @click="startGame">
+      게임 시작
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    startGame () {
+      this.$router.push('/game')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#main {
+}
+.title {
+}
+.startButton {
+}
+</style>
