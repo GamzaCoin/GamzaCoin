b802f0571047bf7583fc0f5eb1eb0d0bb37876dc
Jun Kim <miue22@naver.com>
2018-08-05 15:45:18 +0900

    Chart setting

---
+++ src/components/Chart.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <trend
+        :data="[0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]"
+        :gradient="['black']"
+        smooth>
+    </trend>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Chart'
+}
+</script>
+
+<style>
+
+</style>
