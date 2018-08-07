9aefaed3a1137907f0a373415ad12f20057a9262
박영호 <mellonia.bluerose@gmail.com>
2018-08-08 05:08:31 +0900

    progress bar color gradation

--- src/components/GameFooter.vue
+++ src/components/GameFooter.vue
@@ -2,7 +2,21 @@
   <footer class="game-footer">
     <img class="riding-apeach" v-bind:style="{ left: (timeIndex / graphLength * 100) == 0 ? '-13%' : (timeIndex / graphLength * 100 - 15) + '%'}" src="./../assets/riding-apeach.png" alt="">
     <div class="whole-distance">
-      <div class="remaining-distance" v-bind:style="{ width: (timeIndex / graphLength * 100) + '%' }"></div>
+      <div 
+        class="progress-distance" 
+        v-bind:style="{ width: (timeIndex / graphLength * 100) + '%',
+        backgroundColor: 
+          (timeIndex / graphLength * 100) >= 0 && (timeIndex / graphLength * 100) < 10 ? '#d0ad00' : 
+          (timeIndex / graphLength * 100) >= 10 && (timeIndex / graphLength * 100) < 20 ? '#d1af00' : 
+          (timeIndex / graphLength * 100) >= 20 && (timeIndex / graphLength * 100) < 30 ? '#dfbb00' :
+          (timeIndex / graphLength * 100) >= 30 && (timeIndex / graphLength * 100) < 40 ? '#efc900' :
+          (timeIndex / graphLength * 100) >= 40 && (timeIndex / graphLength * 100) < 50 ? '#fed621' :
+          (timeIndex / graphLength * 100) >= 50 && (timeIndex / graphLength * 100) < 60 ? '#ffd700' :
+          (timeIndex / graphLength * 100) >= 60 && (timeIndex / graphLength * 100) < 70 ? '#ffe524' :
+          (timeIndex / graphLength * 100) >= 70 && (timeIndex / graphLength * 100) < 80 ? '#fff438' :
+          (timeIndex / graphLength * 100) >= 80 && (timeIndex / graphLength * 100) < 90 ? '#ffff4b' : 
+          (timeIndex / graphLength * 100) >= 90 && (timeIndex / graphLength * 100) < 100 ? '#ffff6c' : '#ffff9c'}">
+      </div>
     </div>
     <div class="game-info">
       <img class="money-img" src="./../assets/money.png" alt="">
@@ -61,7 +75,7 @@ export default {
 .riding-apeach {
   width:20%;
   position: absolute;
-  bottom: 120px;
+  bottom: 7.5rem;
   z-index: 900;
   left: 0%;
 }
@@ -75,9 +89,10 @@ export default {
   height: 10px;
   bottom: 7rem;
   z-index: 900;
+  /* opacity: 0.7; */
 }
 
-.remaining-distance {
+.progress-distance {
   background-color: yellow;
   border-radius: 10px;
   border: 1px solid black;
